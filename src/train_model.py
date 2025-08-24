c54124da65cf09f51c3bb8f6254f020c40aeef33
PauCosSicres <pau.cos01@estudiant.upf.edu>
2025-08-24 18:10:48 +0200

    changes

---
+++ src/train_model.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class LogisticReg:
+    def __init__(self):
+        pass
+
+    def sigmoid(self, z):
+        return 1 / (1+ np.exp(-z))
+    
+    def init_params(self, X, W= 0, B= 0):
+        self.weights = np.full(X.shape[1], W)
+        self.bias = B
+        print(f"Parameters initalized with Weights: {W} and Bias: {B}")
+
+    def log_loss(self, y_pred, Y):
+        return -np.mean(Y * np.log(y_pred) + (1-Y)*np.log(1- y_pred))
+        
+    def gradient_descent(self, X, Y, y_pred, loss, lr):
+        m = X.shape[0]
+        dw = (1/m) * np.dot((y_pred - Y), X.T)
+        db = (1/m) * np.sum(y_pred - Y)
+        self.weights -= lr* dw
+        self.bias -= lr* db
+        
+    def fit(self, X, Y, lr= 0.01, n_iter= 100):
+        for i in range(n_iter): 
+            z = np.dot(self.weights, X) + self.bias
+            y_pred = self.sigmoid(z)
+            loss = self.log_loss(y_pred, Y)
+            if i % 25 == 0:
+                print(f"Iteration: {i}\nAccuracy: {1-loss}")
+            if loss < 0.001:
+                break
+        
+            self.gradient_descent(X, Y, y_pred, loss, lr)
+
+    def predict(self, X, threshold = 0.5):
+        z = np.dot(self.weights, X) + self.bias
+        y_pred = self.sigmoid(z)
+        return (y_pred >= threshold).astype(int)
